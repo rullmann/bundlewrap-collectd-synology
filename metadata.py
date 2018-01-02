f06e972b66e9ebe5c83ca15717d74a51558c1595
Rico Ullmann <rico@erinnerungsfragmente.de>
2018-01-02 18:06:26 +0100

    add metadata.py to install required packages

---
+++ metadata.py
@@ -0,0 +1,9 @@
+@metadata_processor
+ def dnf(metadata):
+     if node.has_bundle('dnf'):
+         metadata.setdefault('dnf', {})
+         metadata['dnf'].setdefault('extra_packages', [])
+         for package in ['net-snmp', 'net-snmp-utils']:
+             if package not in metadata['dnf']['extra_packages']:
+                 metadata['dnf']['extra_packages'].append(package)
+     return metadata, DONE
